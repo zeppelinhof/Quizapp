0e6e01ba16cff112cf33dba1d8f3a1a4c21cc781
Markus Gede <markusgede93@gmail.com>
2023-07-28 15:53:06 +0200

    add question

---
+++ script.js
@@ -0,0 +1,34 @@
+let questions = [
+    {
+        "question": "Wie viele Bücher hat der Kanon der Bibel?",
+        "answer_1": "66",
+        "answer_2": "27",
+        "answer_3": "39",
+        "answer_4": "77",
+        "right_answer": 1
+    },
+    {
+        "question": "Wer ist die Wahrheit?",
+        "answer_1": "Mohammed",
+        "answer_2": "Buddha",
+        "answer_3": "Jesus Christus",
+        "answer_4": "ich",
+        "right_answer": 3
+    },
+    {
+        "question": "Welche Person wurde von seinen Brüdern in einen Brunnen geworfen?",
+        "answer_1": "Hesekiel",
+        "answer_2": "Simeon",
+        "answer_3": "Daniel",
+        "answer_4": "Joseph",
+        "right_answer": 4
+    },
+    {
+        "question": "Was schuf Gott am ersten Tag der Schöpfung?",
+        "answer_1": "Tiere",
+        "answer_2": "Licht",
+        "answer_3": "Pflanzen",
+        "answer_4": "Sterne",
+        "right_answer": 2
+    }    
+];
