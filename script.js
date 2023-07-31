b70ea174877a1c65dae9bc38b27882966c8a97c2
Markus Gede <markusgede93@gmail.com>
2023-07-31 08:24:18 +0200

    button colors when true or false

--- script.js
+++ script.js
@@ -41,12 +41,22 @@ function init(){
 }
 
 function showQuestion(){
-    let question = questions[currentQuestion];
-    document.getElementById('questiontext').innerHTML = question['question'];
-    document.getElementById('answer_1').innerHTML = question['answer_1'];
-    document.getElementById('answer_2').innerHTML = question['answer_2'];
-    document.getElementById('answer_3').innerHTML = question['answer_3'];
-    document.getElementById('answer_4').innerHTML = question['answer_4'];
+
+    if (currentQuestion >= question.length){
+
+    }
+
+    else{
+        let question = questions[currentQuestion];
+
+        document.getElementById('question-number').innerHTML = currentQuestion + 1;
+        document.getElementById('questiontext').innerHTML = question['question'];
+        document.getElementById('answer_1').innerHTML = question['answer_1'];
+        document.getElementById('answer_2').innerHTML = question['answer_2'];
+        document.getElementById('answer_3').innerHTML = question['answer_3'];
+        document.getElementById('answer_4').innerHTML = question['answer_4'];
+    }
+    
 }
 
 function answer(selection){
@@ -54,7 +64,7 @@ function answer(selection){
     let selectedQUestionNumber = selection.slice(-1);
     let idOfRightAnswer = `answer_${question['right_answer']}`
 
-    if(idOfRightAnswer == selectedQUestionNumber){
+    if(selectedQUestionNumber == question['right_answer']){
         document.getElementById(selection).parentNode.classList.add('bg-success');
     }
 
@@ -64,4 +74,23 @@ function answer(selection){
     }
 
     document.getElementById('next-button').disabled = false;
+}
+
+function nextQuestion(){
+    currentQuestion++;
+    document.getElementById('next-button').disabled = true;
+    resetAnswerButtons();
+    showQuestion();    
+}
+
+function resetAnswerButtons(){
+    document.getElementById('next-button').disabled = true;
+    document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_1').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-uccess');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-success');
 }
